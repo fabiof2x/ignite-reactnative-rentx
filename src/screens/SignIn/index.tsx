6aff2c676dfe09e8f266a38738fe397766cfdfd8
fabiof2x <fabioar@gmail.com>
2022-02-18 10:57:59 -0300

    Validação de Formulário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,12 +3,14 @@ import {
   StatusBar,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Alert
 } from 'react-native';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
 import theme from '../../styles/theme';
+import * as Yup from 'yup';
 
 import {
   Container,
@@ -22,6 +24,32 @@ import {
 export function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  async function handleSignIn() {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string()
+          .required('A senha é obrigatória')
+      });
+
+      await schema.validate({ email, password });
+      Alert.alert('Tudo certo!');
+      //Fazer login
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer login, verifique as credenciais'
+        )
+      }
+    }
+  }
+
   return (
     <KeyboardAvoidingView behavior='position' enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -64,7 +92,7 @@ export function SignIn() {
           <Footer>
             <Button
               title="Login"
-              onPress={() => { }}
+              onPress={handleSignIn}
               enabled={true}
               loading={false}
             />
