34a9657a7ea1dc5d8a43ffe6c5027de65e6b6cd1
fabiof2x <fabioar@gmail.com>
2022-02-21 17:01:42 -0300

    Listando carros do backend

--- src/screens/SignUp/SignUpFirstStep/index.tsx
+++ src/screens/SignUp/SignUpFirstStep/index.tsx
@@ -23,7 +23,6 @@ import {
   Form,
   FormTitle
 } from './styles';
-import { useAuth } from '../../../hooks/auth';
 
 export function SignUpFirstStep() {
   const [name, setName] = useState('');
@@ -31,8 +30,6 @@ export function SignUpFirstStep() {
   const [driverLicense, setDriverLicense] = useState('');
 
   const navigation = useNavigation<any>();
-  const { user } = useAuth();
-  console.log("USUÁRIO AUTENTICADO", user);
 
   function handleBack() {
     navigation.goBack();
